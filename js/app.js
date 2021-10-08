4498d0730b176df17513a15ee5ead3fe90f7a8bc
branko-stankovic <branko.stankovic@gmail.com>
2021-10-08 20:59:57 +0200

    Call drawGrid onload with a default parameter from input

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 const drawingBoard = document.getElementById('container');
 const gridSize = document.getElementById('gridSize');
 
+function drawGrid(size) {
+    for (let i = 0; i < size; i++) {
+        console.log("DRAWING");
+    }
+}
 
 gridSize.addEventListener('input', function() {
     console.log(gridSize.value);
 });
+
+window.onload = function() {
+    drawGrid(gridSize.value);
+}
